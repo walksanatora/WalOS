07915622a56d204b767fe550eec1212fdddce1d0
walksanatora <walkerffo22@hotmail.com>
2021-01-20 13:54:37 -0800

    smth

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -1,8 +1,10 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_FILE* LoadFile(EFI_FILE* Directory){
-	
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SalSystemTable){
+	EFI_FILE* LoadFile;
+
+	EFI_LOADED_IMAGE_PROTOCOL
 }
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
